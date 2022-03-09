726e97baf3bdd3d006f481433cbcde5472367f62
jon-sousa <jonassleandro1@gmail.com>
2022-03-08 22:13:28 -0300

    configuracoes de deploy

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "development": {
     "username": "root",
     "password": "manu1489",
@@ -14,10 +14,10 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": process.env.database,
+    "host": process.env.host,
+    "dialect": "postgres"
   }
 }
